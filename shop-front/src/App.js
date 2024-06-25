e23fa3ab2d06d6f2cc20acf84b683444feca05d9
miya2009 <miya_2009@naver.com>
2024-06-25 14:11:55 +0900

    no message

---
+++ shop-front/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Admin from './pages/admin/Adimin';
+import NotFound from './pages/not-found/NotFound';
+import UnAuthorized from './pages/unauthorized/UnAuthorized';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/home' element={<Home />}></Route>
+            <Route path='/login' element={<Login />}></Route>
+            <Route path='/register' element={<Register />}></Route>
+            <Route path='/admin' element={<Admin />}></Route>
+            <Route path='/404' element={<NotFound />}></Route>
+            <Route path='/401' element={<UnAuthorized />}></Route>
+            <Route path='*' element={<NotFound />}></Route>
+          </Routes>
+        </div>
+    </BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
